156637a8718b2e82bbac047857c794a63dad4389
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2024-08-24 17:45:58 +0530

    Update backend-practice.js

--- data/backend-practice.js
+++ data/backend-practice.js
@@ -1,7 +1,7 @@
 const xhr = new XMLHttpRequest();
 
 xhr.addEventListener('load', () => {
-    console.log(xhr.response);
+  console.log(xhr.response);
 });
 
 xhr.open('GET', 'https://supersimplebackend.dev');
